f32c5b9802e42bba3a784955fd2e247eb1237657
kpatc <josuekpatcha1@gmail.com>
2025-10-19 17:40:15 +0100

    delete some critical config var

--- src/tests/test_snoflake_conn.py
+++ src/tests/test_snoflake_conn.py
@@ -2,8 +2,8 @@ import snowflake.connector
 
 conn = snowflake.connector.connect(
     account='go08348.af-south-1.aws',
-    user='Josh',
-    password='MalakJosh07112311',
+    user='username',
+    password='password',
     warehouse='COMPUTE_WH',
     database='CDC_DB',
     schema='PUBLIC'
